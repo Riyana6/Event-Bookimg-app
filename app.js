6957091cdd328cff4b7ebba7c1d3f4b18d04b922
JMJ Riyana <jeemariyana@gmail.com>
2020-10-28 11:25:26 +0530

    mongoconnect

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(
 mongoose.connect(
     `mongodb+srv://${process.env.MONGO_USER}:${
         process.env.MONGO_PASSWORD
-    }@cluster0.m3l4e.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+    }@cluster0.m3l4e.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true ,useUnifiedTopology: true }
 ).then(() => {
     app.listen(3000);
 }).catch(err => {
