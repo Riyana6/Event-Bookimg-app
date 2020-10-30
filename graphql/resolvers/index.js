17882057a5418ac5fb9a7aff48b6249445e4754e
JMJ Riyana <jeemariyana@gmail.com>
2020-10-30 13:36:39 +0530

    d

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -21,6 +21,19 @@ const events = async eventIds => {
     };
 };
 
+const singleEvent = async eventId => {
+    try{
+        const event = await Event.findById(eventId);
+        return {
+            ...event._doc,
+            _id: event.id,
+            creator: user.bind(this, event.creator)
+        };  
+    } catch (err) {
+        throw err;
+    }
+}
+
 const user = async userId => {
     try {
     const user = await User.findById(userId)
@@ -57,6 +70,8 @@ module.exports = {
                 return { 
                     ...booking._doc, 
                     _id: booking.id, 
+                    user: user.bind(this, booking._doc.user),
+                    event: singleEvent.bind(this, booking._doc.event),
                     createdAt: new Date(booking._doc.createdAt).toISOString(),
                     updatedAt: new Date(booking._doc.updatedAt).toISOString()
                 };
@@ -117,8 +132,17 @@ module.exports = {
         }     
     },
     bookEvent: async args => {
+        const fetchedEvent = await Event.findOne({_id: args.eventId});
         const booking = new Booking({
-
+            user: '5f9bab7820bc3224e4a94c7d',
+            event: fetchedEvent
         });
+        const result = await booking.save();
+        return {
+            ...result._doc, 
+            _id: result.id, 
+            createdAt: new Date(result._doc.createdAt).toISOString(),
+            updatedAt: new Date(result._doc.updatedAt).toISOString()
+        };
     }
 }
