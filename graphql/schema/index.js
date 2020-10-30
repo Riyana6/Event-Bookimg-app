d61919dad04b5096321c6da571b830d8d752b9dc
JMJ Riyana <jeemariyana@gmail.com>
2020-10-30 12:36:15 +0530

    d

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,45 +1,52 @@
 const {buildSchema} = require('graphql');
 
 module.exports = buildSchema (`
-type Event {
-    _id: ID!
-    title: String!
-    description: String!
-    price: Float!
-    date: String!
-    creator: User!
-}
+    type Booking {
+        event: Event!
+        user: User!
+        createdAt: String!
+        updatedAt: String!
+    }
 
-type User {
-    _id: ID!
-    email: String!
-    password: String!
-    createdEvents: [Event!]!
-}
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+        creator: User!
+    }
 
-input EventInput {
-    title: String!
-    description: String!
-    price: Float!
-    date: String!
-}
+    type User {
+        _id: ID!
+        email: String!
+        password: String!
+        createdEvents: [Event!]!
+    }
 
-input UserInput {
-    email: String!
-    password: String!
-}
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
 
-type RootQuery {
-    events: [Event!]!
-}
+    input UserInput {
+        email: String!
+        password: String!
+    }
 
-type RootMutation {
-    createEvent(eventInput: EventInput): Event
-    createUser(userInput: UserInput): User
-}
+    type RootQuery {
+        events: [Event!]!
+    }
 
-schema {
-    query: RootQuery
-    mutation:RootMutation
-}
+    type RootMutation {
+        createEvent(eventInput: EventInput): Event
+        createUser(userInput: UserInput): User
+    }
+
+    schema {
+        query: RootQuery
+        mutation:RootMutation
+    }
 `);
